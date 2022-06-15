2df7fde5f4d1104e45e4ec34d4781ff08b3592d6
Conal97 <conalbrosnan@gmail.com>
2022-06-16 00:05:07 +0100

    Completed lesson 2. Kotlin doesn't have a ternary so used if instead, as if returns a value in Kotlin.

--- src/main/kotlin/com/conalbrosnan/ipldashboard/data/MatchDataProcessor.kt
+++ src/main/kotlin/com/conalbrosnan/ipldashboard/data/MatchDataProcessor.kt
@@ -12,7 +12,7 @@ class MatchDataProcessor: ItemProcessor<MatchInput, Match> {
 
     val log:Logger = LoggerFactory.getLogger(javaClass.enclosingClass)
 
-    override fun process(matchInput: MatchInput): Match? {
+    override fun process(matchInput: MatchInput): Match {
 
         val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
 
@@ -20,17 +20,37 @@ class MatchDataProcessor: ItemProcessor<MatchInput, Match> {
         lateinit var secondInningsTeam: String
 
         // Determine team1 and team2 based on innings order
-        if ("bat".equals(matchInput.toss_decision)){
+        if ("bat" == matchInput.toss_decision) {
             firstInningsTeam = matchInput.toss_winner
-            //secondInningsTeam = matchInput.toss_winner.equals(matchInput.team1 ? )
+            secondInningsTeam = if (matchInput.toss_winner == matchInput.team1) {
+                matchInput.team2
+            } else {
+                matchInput.team1
+            }
+        } else {
+            secondInningsTeam = matchInput.toss_winner
+            firstInningsTeam = if (matchInput.toss_winner == matchInput.team1) {
+                matchInput.team2
+            } else {
+                matchInput.team1
+            }
         }
 
-        val match = Match (
+        return Match(
             id = matchInput.id.toLong(),
             city = matchInput.city,
             date = LocalDate.parse(matchInput.date, formatter),
             playerOfMatch = matchInput.player_of_match,
-            team1 =
-        )
+            venue = matchInput.venue,
+            team1 = firstInningsTeam,
+            team2 = secondInningsTeam,
+            tossWinner = matchInput.toss_winner,
+            tossDecision = matchInput.toss_decision,
+            matchWinner = matchInput.winner,
+            result = matchInput.result,
+            resultMargin = matchInput.result_margin,
+            umpire1 = matchInput.umpire1,
+            umpire2 = matchInput.umpire2
+        );
     }
 }
